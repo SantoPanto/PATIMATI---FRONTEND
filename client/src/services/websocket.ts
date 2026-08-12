f0bfdf6529fae1314117795407ceb7e90eca7db9
SenaF116 <153498406+SenaF116@users.noreply.github.com>
2026-08-12 17:55:47 +0300

    Update websocket.ts

--- client/src/services/websocket.ts
+++ client/src/services/websocket.ts
@@ -1,5 +1,5 @@
 import { Client } from "@stomp/stompjs";
-import SockJS from "sockjs-client";
+import SockJS from "sockjs-client/dist/sockjs.js";
 
 import { API_BASE_URL } from "./api";
 import { getStoredToken } from "./auth";
